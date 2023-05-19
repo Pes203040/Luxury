73c95c6be090e3d27c679b1887c5fe1852b107cb
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2023-05-19 19:46:01 +0300

    Update Luxury.py

--- Luxury.py
+++ Luxury.py
@@ -1,7 +1,6 @@
 import os, sys
-os.system("git pull")
-os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
+os.system('git pull')
 try:
-    __import__("NCYR").menu()
+    __import__("NYC").menu()
 except Exception as e:
     exit(str(e))
